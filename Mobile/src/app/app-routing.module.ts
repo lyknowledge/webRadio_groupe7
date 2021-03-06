af883a2666ea6ea101f690d6fd0e4a0dc80ac1cd
Ulrich-Nelson <ulrichnelsontapinfo@yahoo.fr>
2021-03-06 15:25:00 +0100

    creation of the different pages of the application

--- Mobile/src/app/app-routing.module.ts
+++ Mobile/src/app/app-routing.module.ts
@@ -3,12 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./pages/registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+ 
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
---
+++ Mobile/src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children:[
+      //chargement de la page permettant de consulter son profil
+      {
+        path: 'profil',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../profil/profil.module').then( m => m.ProfilPageModule)
+          },
+        ]
+      },
+      //chargement de la page permettant d'écouter la radio
+      {
+        path: 'radio',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../radio/radio.module').then(m => m.RadioPageModule)
+          },
+        ]
+      },
+
+      // chargement de la page permettant d 'écouter les musiques en favorite
+      {
+        path: 'favorites',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../favorites/favorites.module').then(m => m.FavoritesPageModule)
+          },
+        ]
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/profil',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
